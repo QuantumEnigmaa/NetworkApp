c106514f8afb315b26376c9e60a47114fb62fd0f
QuantumEnigmaa <thibaud.vaisseau@isen.yncrea.fr>
2021-02-08 00:00:57 +0100

    Implémentation choix photo dans galleroe

--- app/src/main/java/fr/isen/networkapp/pk/PostActivity.kt
+++ app/src/main/java/fr/isen/networkapp/pk/PostActivity.kt
@@ -1,15 +1,25 @@
 package fr.isen.networkapp.pk
 
+import android.annotation.TargetApi
 import android.app.Activity
+import android.app.ProgressDialog.show
 import android.content.ActivityNotFoundException
+import android.content.ContentUris
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.provider.MediaStore
+import android.util.Log
 import fr.isen.networkapp.pk.databinding.ActivityPostBinding
 import android.view.View
 import android.widget.ImageView
+import com.squareup.picasso.Picasso
+import fr.isen.networkapp.pk.extensions.Extensions.toast
 import fr.isen.networkapp.pk.model.Post
 import fr.isen.networkapp.pk.utils.FirebaseUtils.dbRef
 import fr.isen.networkapp.pk.utils.FirebaseUtils.firebaseUser
@@ -17,15 +27,20 @@ import fr.isen.networkapp.pk.utils.FirebaseUtils.firebaseUser
 
 class PostActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostBinding
-    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Taking a picture
         binding.newPictureButton.setOnClickListener {
             dispatchTakePictureIntent()
+        }
+
+        // Choosing picture from phone
+        binding.addPictureButton.setOnClickListener {
+            loadChosenPicture()
         }
 
         binding.postButton.setOnClickListener {
@@ -37,11 +52,25 @@ class PostActivity : AppCompatActivity() {
         }
     }
 
+    private fun loadChosenPicture() {
+        val choosePictureIntent = Intent().setAction(Intent.ACTION_GET_CONTENT)
+        choosePictureIntent.type = "image/*"
+        startActivityForResult(Intent.createChooser(choosePictureIntent, "choisissez une image"), REQUEST_IMAGE_CHOOSE)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.imageView.setImageBitmap(imageBitmap)
+        when (requestCode) {
+            REQUEST_IMAGE_CAPTURE ->
+                if (resultCode == RESULT_OK) {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    binding.imageView.setImageBitmap(imageBitmap)
+                }
+            REQUEST_IMAGE_CHOOSE ->
+                if (resultCode == RESULT_OK) {
+                    val filepath = data?.data
+                    Picasso.get().load(filepath).into(binding.imageView)
+                }
         }
     }
 
@@ -50,10 +79,13 @@ class PostActivity : AppCompatActivity() {
         try {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
         } catch (e: ActivityNotFoundException) {
-            // display error state to the user
+            e.printStackTrace()// display error state to the user
         }
     }
 
-
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_IMAGE_CHOOSE = 2
+    }
 
 }
